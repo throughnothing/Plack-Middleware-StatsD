8fc94157f7c1499b41344c67b2d7e51bad327a63
William Wolf <throughnothing@gmail.com>
2013-07-31 14:20:20 -0700

    Cleanup

--- lib/Plack/Middleware/StatsD.pm
+++ lib/Plack/Middleware/StatsD.pm
@@ -1,6 +1,6 @@
+package Plack::Middleware::StatsD;
 use strict;
 use warnings;
-package Plack::Middleware::StatsD;
 use parent qw(Plack::Middleware);
 use Etsy::StatsD;
 use Plack::Request;
@@ -9,6 +9,7 @@ use Plack::Util::Accessor qw(
     host port sample_rate prefix stat_name_mapper whitelist
     _sd
 );
+use Sys::Hostname qw( hostname );
 use Time::HiRes qw( time );
 
 # ABSTRACT: Plack middleware for logging request/response data with StatsD
@@ -23,8 +24,13 @@ sub prepare_app {
     $self->sample_rate( 1 ) unless defined $self->sample_rate;
 
     $self->whitelist( sub{ 1 } ) unless ref $self->whitelist eq "CODE";
-    $self->stat_name_mapper( sub{ shift->path } )
-        unless ref $self->stat_name_mapper eq "CODE";
+    # By Default, replace '/' with '_' in request path.
+    $self->stat_name_mapper(sub{
+        my ($req, $res) = @_;
+        # Replace '/' with '_' and '.' with '-'
+        my $path = $req->path =~ s{/}{_}r =~ s{\.}{-}r;
+        return join '.', hostname, $path, uc( $req->method ), $res->[0] ;
+    }) unless ref $self->stat_name_mapper eq "CODE";
 
     my $sd = Etsy::StatsD->new( $self->host, $self->port, $self->sample_rate );
     $self->_sd( $sd )
@@ -40,26 +46,20 @@ sub call {
     my $res = $self->app->($env);
     Plack::Util::response_cb($res, sub {
         my ($res) = @_;
-
+        my $end = time;
         return $res unless $self->whitelist->( $req );
 
-        my ($end, $code) = ( time, $res->[0]);
-        my $path = $self->stat_name_mapper->( $req );
-
+        my $path = $self->stat_name_mapper->( $req, $res );
         # Log the response time ( in ms )
-        my $time = ( $end - $start ) * 1000;
-        $self->_sd->timing( $self->_stat( "$path.time" ), $time );
-
-        # Increment code counter
-        $self->_sd->increment( $self->_stat( $code ) );
-        # Increment code + path hcounter
-        $self->_sd->increment( $self->_stat( "$path.$code" ) );
+        $self->_sd->timing( $self->_stat( $path ), ( $end - $start ) * 1000 );
+        # Increment response counter
+        $self->_sd->increment( $self->_stat( $path ) );
 
         return $res;
     });
 }
 
-sub _stat { shift->prefix . '.' . shift }
+sub _stat { join '.', shift->prefix, @_ }
 
 1;
 
@@ -82,7 +82,7 @@ sub _stat { shift->prefix . '.' . shift }
             # Return true to process, false to ignore
         },
         stat_name_mapper => sub {
-            my ($req) = @_;
+            my ($req, $res) = @_;
             ...
             # Return what to name the stat for this request
             return $req->path;
